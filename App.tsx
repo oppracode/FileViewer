7daadd9d43e315d7d6b009924f0d0f9d2201f2a4
Rostyslav Sivirin <oppracode@gmail.com>
2024-01-20 19:30:00 +0200

    Created toolbar navigation, moved content area's code to a screen

--- App.tsx
+++ App.tsx
@@ -1,20 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {Ionicons} from '@expo/vector-icons'
+
+import FileViewerScreen from './screens/FileViewerScreen'
+import Toolbar from './components/Toolbar';
 
 export default function App() {
+  const Tab = createBottomTabNavigator();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Toolbar/>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
