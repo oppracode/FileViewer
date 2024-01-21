1b632bf4db56ccaa674d0a8e7e6de8548f34c25b
Rostyslav Sivirin <oppracode@gmail.com>
2024-01-21 18:35:09 +0200

    Added 'loading' icon for when user waits for api call to return information

--- components/FileArea.tsx
+++ components/FileArea.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect } from 'react';
-import { View, Text, FlatList, StyleSheet, Dimensions } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, FlatList, StyleSheet, Dimensions, ActivityIndicator } from 'react-native';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchFiles } from '../features/dropboxSlice';
 import { RootState } from '../store/store';
@@ -8,42 +8,60 @@ import FileCard from './ItemCard';
 
 const { width, height } = Dimensions.get('window');
 interface DropboxFile {
-    name: string;
-    id: string;
-    ".tag": string; 
-  }
-
+  name: string;
+  id: string;
+  ".tag": string;
+}
 
 const FileArea: React.FC = () => {
   const dispatch = useDispatch();
   const dropboxFiles = useSelector((state: RootState) => state.dropbox.files);
 
-  useEffect(() => {
-    dispatch(fetchFiles() as any); 
-  }, [dispatch]);
+  const [loading, setLoading] = useState(true);
 
-  const renderItem = ({ item }) => <FileCard {...item} />;
+  useEffect(() => {
+    dispatch(fetchFiles() as any)
+      .then(() => {
+        setLoading(false);
+      })
+      .catch((error: Error) => {
+        console.error('API call error:', error);
+        setLoading(false);
+      });
+  }, [dispatch]);
+  
+
+  const renderItem = ({ item }: { item: DropboxFile }) => <FileCard {...item} />;
   const files = dropboxFiles.filter((file) => file[".tag"] === 'file');
-  const folders = dropboxFiles.filter((file) => file[".tag"] === 'folder')
+  const folders = dropboxFiles.filter((file) => file[".tag"] === 'folder');
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>FILES: {files.length}</Text>
       <View style={styles.fileListContainer}>
-        <FlatList
-          data={files}
-          renderItem={renderItem}
-          keyExtractor={(_, index) => index.toString()}
-          numColumns={3}  
-        />
+        {loading ? (
+          <ActivityIndicator size="large" color="#0000ff" />
+        ) : (
+          <FlatList
+            data={files}
+            renderItem={renderItem}
+            keyExtractor={(_, index) => index.toString()}
+            numColumns={3}
+          />
+        )}
       </View>
       <Text style={styles.title}>FOLDERS: {folders.length}</Text>
       <View style={styles.fileListContainer}>
-        <FlatList
-          data={folders}
-          renderItem={renderItem}
-          keyExtractor={(_, index) => index.toString()}
-          numColumns={3} 
-        />
+        {loading ? (
+          <ActivityIndicator size="large" color="#0000ff" />
+        ) : (
+          <FlatList
+            data={folders}
+            renderItem={renderItem}
+            keyExtractor={(_, index) => index.toString()}
+            numColumns={3}
+          />
+        )}
       </View>
     </View>
   );
@@ -60,13 +78,13 @@ const styles = StyleSheet.create({
     marginBottom: 10,
   },
   fileListContainer: {
-    width: width,  
+    width: width,
     padding: 16,
-    height: '40%', 
+    height: '40%',
     borderWidth: 1,
     borderColor: 'gray',
     borderRadius: 8,
-    overflow: 'hidden',  
+    overflow: 'hidden',
   },
 });
 
