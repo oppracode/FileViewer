56eefa3240c6445b93a79d9dd8fa2ca2bb9b9fa9
Rostyslav Sivirin <oppracode@gmail.com>
2024-01-22 03:35:30 +0200

    Remove unused code

--- src/components/DropboxItem.tsx
+++ src/components/DropboxItem.tsx
@@ -2,13 +2,11 @@ import React, { useEffect } from 'react';
 import { View, Text } from 'react-native';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchFiles } from '../features/dropboxSlice';
-import { RootState } from '../store/store';
 
 import FileArea from './FileArea';
 
 const DropboxItem: React.FC = () => {
   const dispatch = useDispatch();
-  const dropboxFiles = useSelector((state: RootState) => state.dropbox.files);
 
   useEffect(() => {
     dispatch(fetchFiles() as any);
