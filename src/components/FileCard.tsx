572b5eec52b3cf5ecec40e151e2881bf9426d27d
Rostyslav Sivirin <oppracode@gmail.com>
2024-01-23 00:59:54 +0200

    Added context menu

--- src/components/FileCard.tsx
+++ src/components/FileCard.tsx
@@ -1,58 +1,59 @@
 import React from '@react-navigation/native';
-import { Button, Pressable, StyleSheet, Text, View } from 'react-native';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
 import { FontAwesome6 } from '@expo/vector-icons';
 import { DropboxFile, FileType } from '../types';
-import { useDispatch } from 'react-redux';
-import { AppDispatch } from '../store/store';
-import { deleteFile, fetchFiles } from '../features/dropboxSlice';
+import { useState } from 'react';
+import { FileModal } from './FileModal';
 
 export function FileCard({ file }: { file: DropboxFile }) {
-  const dispatch: AppDispatch = useDispatch();
-  const handlePress = () => {
-    dispatch(deleteFile(file.path));
-    console.log(`deleted: ${file.path}`);
-    dispatch(fetchFiles() as any);
-  };
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const closeModal = () => setModalVisible(false);
 
   return (
-    <Pressable
-      style={({ pressed }) => [
-        {
-          ...styles.pressable,
-          backgroundColor: pressed ? '#eee' : '#fff',
-        },
-      ]}
-      //onPress={handlePress}
-    >
-      <View style={styles.content}>
-        <View style={styles.fileIcon}>
-          <FontAwesome6
-            name={file.type == FileType.FILE ? 'file' : 'folder'}
-            size={32}
-          />
-        </View>
-        <View style={styles.descriptionContainer}>
-          <Text ellipsizeMode='tail' numberOfLines={1} style={styles.title}>
-            {file.name}
-          </Text>
-          {file.clientModified != null && (
-            <Text style={styles.description}>
-              {new Date(file.clientModified!).toLocaleString()}
-            </Text>
-          )}
-        </View>
-      </View>
+    <>
       <Pressable
         style={({ pressed }) => [
           {
-            ...styles.button,
-            backgroundColor: pressed ? '#eee' : 'transparent',
+            ...styles.pressable,
+            backgroundColor: pressed ? '#eee' : '#fff',
           },
         ]}
+        onLongPress={() => setModalVisible(true)}
       >
-        <FontAwesome6 name='ellipsis' size={24} color='#0d0d0d' />
+        <View style={styles.content}>
+          <View style={styles.fileIcon}>
+            <FontAwesome6
+              name={file.type == FileType.FILE ? 'file' : 'folder'}
+              size={32}
+            />
+          </View>
+          <View style={styles.descriptionContainer}>
+            <Text ellipsizeMode='tail' numberOfLines={1} style={styles.title}>
+              {file.name}
+            </Text>
+            {file.clientModified != null && (
+              <Text style={styles.description}>
+                {new Date(file.clientModified!).toLocaleString()}
+              </Text>
+            )}
+          </View>
+        </View>
+        <Pressable
+          style={({ pressed }) => [
+            {
+              ...styles.button,
+              backgroundColor: pressed ? '#eee' : 'transparent',
+            },
+          ]}
+          onPress={() => setModalVisible(true)}
+        >
+          <FontAwesome6 name='ellipsis' size={24} color='#0d0d0d' />
+        </Pressable>
       </Pressable>
-    </Pressable>
+
+      <FileModal isVisible={modalVisible} onClose={closeModal} file={file} />
+    </>
   );
 }
 
---
+++ src/components/FileModal.tsx
@@ -0,0 +1,140 @@
+import React from '@react-navigation/native';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
+import { FontAwesome6 } from '@expo/vector-icons';
+import { DropboxFile } from '../types';
+import { useDispatch } from 'react-redux';
+import { AppDispatch } from '../store/store';
+import { deleteFile, fetchFiles } from '../features/dropboxSlice';
+
+import Modal from 'react-native-modal';
+
+export function FileModal({
+  isVisible,
+  onClose,
+  file,
+}: {
+  isVisible: boolean;
+  onClose: () => void;
+  file: DropboxFile;
+}) {
+  const dispatch: AppDispatch = useDispatch();
+
+  const deleteSelectedFile = () => {
+    dispatch(deleteFile(file.path));
+    onClose();
+    dispatch(fetchFiles() as any);
+  };
+
+  return (
+    <>
+      <Modal
+        testID={'modal'}
+        isVisible={isVisible}
+        onSwipeComplete={onClose}
+        onBackdropPress={onClose}
+        onBackButtonPress={onClose}
+        backdropOpacity={0.45}
+        swipeDirection={['down']}
+        style={{
+          justifyContent: 'flex-end',
+          margin: 0,
+        }}
+      >
+        <View
+          style={{
+            backgroundColor: 'white',
+            paddingVertical: 16,
+            justifyContent: 'flex-start',
+            alignItems: 'center',
+            borderRadius: 4,
+            borderColor: 'rgba(0, 0, 0, 0.1)',
+          }}
+        >
+          <FileModalAction
+            icon='trash-can'
+            iconColor='red'
+            title='Delete'
+            onPress={deleteSelectedFile}
+          />
+          <View style={styles.actionSeparator} />
+          <FileModalAction
+            icon=''
+            iconColor='#0d0d0d'
+            title='Close'
+            onPress={onClose}
+            useIcon={false}
+          />
+        </View>
+      </Modal>
+    </>
+  );
+}
+
+function FileModalAction({
+  title,
+  icon,
+  useIcon = true,
+  iconColor,
+  onPress,
+}: {
+  title: string;
+  icon: string;
+  useIcon?: boolean;
+  iconColor: string;
+  onPress: () => void;
+}) {
+  return (
+    <Pressable
+      style={({ pressed }) => [
+        {
+          ...styles.actionView,
+          backgroundColor: pressed ? '#eee' : '#fff',
+        },
+      ]}
+      onPress={onPress}
+    >
+      <View style={styles.pressableContentView}>
+        {useIcon && (
+          <View style={styles.actionIcon}>
+            <FontAwesome6 name={icon} size={24} color={iconColor} />
+          </View>
+        )}
+        <Text style={styles.actionTitle}>{title}</Text>
+      </View>
+    </Pressable>
+  );
+}
+
+const styles = StyleSheet.create({
+  actionView: {
+    width: '100%',
+    flexDirection: 'row',
+    paddingVertical: 12,
+    paddingHorizontal: 24,
+    minHeight: 40,
+  },
+  actionSeparator: {
+    width: '100%',
+    height: 1.25,
+    backgroundColor: '#eee',
+  },
+  pressableContentView: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    columnGap: 12,
+    flex: 1,
+  },
+  actionTitle: {
+    fontSize: 16,
+    lineHeight: 20,
+    fontWeight: '400',
+    color: '#0d0d0dFF',
+  },
+  actionIcon: {
+    width: 36,
+    height: 36,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
